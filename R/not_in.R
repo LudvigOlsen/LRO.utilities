e57f1b984bd5dea9d86749d62c844570a8c0e0cf
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2017-03-15 19:13:20 +0100

    Wrote function instead of just negating %in% as it wasn't recognized as an object to export. Documentation.

--- R/not_in.R
+++ R/not_in.R
@@ -1,6 +1,15 @@
 # Not in
 
-#' @title Not in
-#' @description Negated %in%.
+#' @title Values Not In
+#' @description Negated \code{\%in\%}.
+#' @param x Vector with values to be matched.
+#' @param table Vector with values to be matched against.
 #' @export
-`%ni%` <- Negate(`%in%`)
+#' @return A logical vector, indicating if no match
+#'  was located for each element of x.
+#'  Values are TRUE or FALSE and never NA.
+`%ni%` <- function(x, table) {
+
+  return(!(x %in% table))
+
+  }
