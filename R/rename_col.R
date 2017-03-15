954f8f0e2639303d716cb1c1bcb793726bb38123
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2017-03-15 19:49:59 +0100

    Documented rename_col

--- R/rename_col.R
+++ R/rename_col.R
@@ -1,3 +1,10 @@
+#' @title Rename single column by name
+#' @description Change name of a single column in a dataframe by old name instead of position.
+#' @param data Dataframe.
+#' @param old_name Name of column to rename (Character).
+#' @param new_name Desired name of column (Character).
+#' @return Dataframe with modified name of specified column.
+#' @export
 rename_col <- function(data, old_name, new_name){
 
   #
@@ -7,3 +14,5 @@ rename_col <- function(data, old_name, new_name){
   return(data)
 
 }
+
+
