968340f1d181ecb0d17beecf549e0eb58cd5d0ab
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2017-06-16 08:24:05 +0200

    Added digits argument for rounding. Function code is very ugly.

--- R/maxap.R
+++ R/maxap.R
@@ -7,10 +7,11 @@
 #' @param data Dataframe with samples.
 #'  A column for each parameter
 #' @param pars Names of parameters (Character).
-#' @param HPDI_prob Probability for highest posterior density interval
+#' @param HPDI_prob Probability for highest posterior density interval (0-1).
+#' @param digits How many digits to round to (Whole number).
 #' @export
 #' @importFrom dplyr '%>%'
-maxap <- function(data, pars = c(), HPDI_prob = 0.89){
+maxap <- function(data, pars = c(), HPDI_prob = 0.89, digits = NULL){
 
   plyr::ldply(1:length(pars), function(p){
 
@@ -23,12 +24,20 @@ maxap <- function(data, pars = c(), HPDI_prob = 0.89){
     colnames(HPDI_) <- HPDI_names
     rownames(HPDI_) <- NULL
 
-    data.frame('Parameter' = pars[p],
+    df <- data.frame('Parameter' = pars[p],
                'MAP' = density(data[[pars[p]]])$x[which(
                  density(data[[pars[p]]])$y==max(
                    density(data[[pars[p]]])$y))]) %>%
       tibble::as_tibble() %>%
       dplyr::bind_cols(HPDI_)
+
+    if (is.null(digits)) {
+      return(df)
+    } else {
+      stopifnot(is.numeric(digits))
+      df[,-1] <-round(df[,-1], digits = digits)
+      return(df)
+    }
 
   })
 
